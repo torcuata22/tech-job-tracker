7b53d4f302742e1cdd933a498873965a81555969
torcuata22 <marilynjmarquez@gmail.com>
2024-05-06 12:38:02 -0400

    model

--- techjobs/companies/models.py
+++ techjobs/companies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Company(models.Model):
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = "Layoffs"
+        HIRING_FREEZE = "Hiring Freeze"
+        HIRING = "Hiring"
+        
+    name = models.CharField(max_length=50, unique=True, null=False)
+    status = models.CharField(max_length=50 ,choices = CompanyStatus.choices, default=CompanyStatus.HIRING)
+    lastt_update = models.DateTimeField(default = now, editable=True)
+    notes = models.CharField(max_length=100, blank=True)
+    
